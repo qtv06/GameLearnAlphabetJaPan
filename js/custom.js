9c09e0a64fcc97fa263f73c7318f3b40b3e0a407
vovanquang12cntt <vanquang451@gmail.com>
2018-09-07 16:15:42 +0700

    answer word

---
+++ js/custom.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+  var listWord = [
+    {
+      tv: 'a',
+      tn: 'あ'
+    },
+    {
+      tv: 'i',
+      tn: 'い'
+    },
+    {
+      tv: 'u',
+      tn: 'う'
+    },
+    {
+      tv: 'e',
+      tn: 'え'
+    },
+    {
+      tv: 'o',
+      tn: 'お'
+    },
+  ];
+  var wordCurrent, numCorrect = 0, totalWord;
+  totalWord = listWord.length;
+  setWord();
+
+  function sendAnswer() {
+    var answer = document.getElementById('resultInput').value;
+    var p = document.getElementById('mess');
+    if(answer === wordCurrent.tv){
+      p.innerHTML = "Right";
+      numCorrect += 1;
+    }else{
+      p.innerHTML = "Wrong!!";
+    }
+    document.getElementById('resultInput').value = "";
+    setWord();
+  }
+
+  function getRandom() {
+    var random = Math.floor(Math.random() * totalWord-1) + 1;
+    return random;
+  }
+
+  function setWord() {
+    wordCurrent = listWord[getRandom()];
+    $('#textShow').attr('data-key', wordCurrent.tv);
+    $('#textShow').text(wordCurrent.tn);
+    $('.present').text(numCorrect);
+    $('.actually').text(totalWord);
+  }
+  // readFile('bangchucai.txt');
+
+  // function readFile(nameFile) {
+  //   var xhttp = new XMLHttpRequest();
+  //   xhttp.onreadystatechange = function() {
+  //     var data = this.responseText;
+  //     console.log(data);
+  //   }
+  //   xhttp.open("get", nameFile, true);
+  //   xhttp.send();
+  // }
+  document.getElementById('resultInput').onkeypress = function(e) {
+    var event = e || window.event;
+    var charCode = event.which || event.code;
+    if(charCode == '13'){
+      sendAnswer();
+      return false;
+    }
+  }
+});
